70e7ce3fa0b9a7440eef027b6592681880f21c00
om3r2477-del <om3r2477@gmail.com>
2026-06-27 22:30:50 +0400

    add first project

---
+++ firstCsharpProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace firstCsharpProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
